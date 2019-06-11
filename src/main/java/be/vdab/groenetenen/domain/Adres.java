75483c7caf546b1961f24d83a3679b13b1bf31bb
pietergilles <gillespieter@hotmail.com>
2019-06-11 15:58:36 +0200

    First Commit

---
+++ src/main/java/be/vdab/groenetenen/domain/Adres.java
@@ -0,0 +1,55 @@
+package be.vdab.groenetenen.domain;
+
+import org.hibernate.validator.constraints.Range;
+
+import javax.persistence.Embeddable;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.io.Serializable;
+
+@Embeddable
+public class Adres implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @NotBlank
+    private String straat;
+    @NotBlank
+    private String huisNr;
+    @NotNull
+    @Range(min = 1000, max = 9999)
+    private int postcode;
+    @NotBlank
+    private String gemeente;
+// Je maakt getters voor alle variabelen (behalve serialVersionUID)
+
+    public String getStraat() {
+        return straat;
+    }
+
+    public void setStraat(String straat) {
+        this.straat = straat;
+    }
+
+    public String getHuisNr() {
+        return huisNr;
+    }
+
+    public void setHuisNr(String huisNr) {
+        this.huisNr = huisNr;
+    }
+
+    public int getPostcode() {
+        return postcode;
+    }
+
+    public void setPostcode(int postcode) {
+        this.postcode = postcode;
+    }
+
+    public String getGemeente() {
+        return gemeente;
+    }
+
+    public void setGemeente(String gemeente) {
+        this.gemeente = gemeente;
+    }
+}
