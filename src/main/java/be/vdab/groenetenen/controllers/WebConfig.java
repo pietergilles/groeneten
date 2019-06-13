281bc8cf98149c959317cd36ee3a9da1e5f404d0
Asterion <moonsovermowedmeadows@hotmail.com>
2019-06-13 11:59:23 +0200

    7 MEERTALIGE WEBSITES

---
+++ src/main/java/be/vdab/groenetenen/controllers/WebConfig.java
@@ -0,0 +1,27 @@
+package be.vdab.groenetenen.controllers;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.FixedLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
+
+@Configuration
+class WebConfig implements WebMvcConfigurer
+{
+    @Bean
+    CookieLocaleResolver localeResolver() {
+        CookieLocaleResolver resolver = new CookieLocaleResolver();
+        resolver.setCookieMaxAge(604_800); 
+        return resolver;
+    }
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LocaleChangeInterceptor());
+    }
+}
